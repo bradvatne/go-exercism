06b97a9a6cea2bd35cf337f7c6e3bc824ac5a4a0
Brad Vatne <brad.vatne@gmail.com>
2023-07-20 10:34:50 +0000

    scrabble score

--- weather-forecast/weather_forecast.go
+++ weather-forecast/weather_forecast.go
@@ -1,8 +1,13 @@
+
+//Package weather contains forecast 
 package weather
 
+//CurrentCondition: tells us about the current weather for the location
 var CurrentCondition string
+//CurrentLocation: is the current location for the forecast
 var CurrentLocation string
 
+//Forecast : is a function that returns a string with the weather forecast details
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
